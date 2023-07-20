76905436ba98e8819e7a76a57995da160e2292c9
tedythsu <ted.yt.hsu@gmail.com>
2023-07-21 02:04:48 +0800

    Update Login Form Validation Logic

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators, FormGroup } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+
+interface loginFormErrorTips {
+  account: string;
+  userName: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -7,15 +13,45 @@ import { UntypedFormBuilder, UntypedFormGroup, Validators, FormGroup } from '@an
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  validateForm!: UntypedFormGroup;
+  loginForm!: UntypedFormGroup;
+  accountVisible = false;
+  userNameVisible = false;
   passwordVisible = false;
-  password?: string;
+
+  loginFormErrorTips: loginFormErrorTips = {
+    account: '必填欄位',
+    userName: '請輸入6-20碼英數符號',
+    password: '請輸入8-12碼英數符號'
+  }
+
+  subscribeToFormChanges() {
+    this.loginForm.valueChanges.subscribe(() => {
+      this.onFormValueChanged();
+    });
+  }
+
+  onFormValueChanged() {
+    const userNameControl = this.loginForm.get('userName');
+    const passwordControl = this.loginForm.get('password');
+
+    if (userNameControl?.value !== "" && userNameControl?.invalid) {
+      this.loginFormErrorTips.userName = "輸入長度不符合規則";
+    } else {
+      this.loginFormErrorTips.userName = "請輸入6-20碼英數符號";
+    }
+
+    if (passwordControl?.value !== "" && passwordControl?.invalid) {
+      this.loginFormErrorTips.password = "輸入長度不符合規則";
+    } else {
+      this.loginFormErrorTips.password = "請輸入6-20碼英數符號";
+    }
+  }
 
   submitForm(): void {
-    if (this.validateForm.valid) {
-      console.log('submit', this.validateForm.value);
+    if (this.loginForm.valid) {
+      console.log('submit', this.loginForm.value);
     } else {
-      Object.values(this.validateForm.controls).forEach(control => {
+      Object.values(this.loginForm.controls).forEach(control => {
         if (control.invalid) {
           control.markAsDirty();
           control.updateValueAndValidity({ onlySelf: true });
@@ -27,11 +63,14 @@ export class LoginComponent implements OnInit {
   constructor(private fb: UntypedFormBuilder) { }
 
   ngOnInit(): void {
-    this.validateForm = this.fb.group({
-      userName: [null, [Validators.required]],
-      password: [null, [Validators.required]],
-      remember: [true]
-    });
+    this.loginForm = this.fb.group({
+      account: [null, [Validators.required]],
+      userName: [null, [Validators.required, Validators.pattern(/^[a-zA-Z0-9]{6,20}$/)]],
+      password: [null, [Validators.required, Validators.pattern(/^[a-zA-Z0-9]{8,12}$/)]],
+      remember: [false],
+    }, { updateOn: 'blur' });
+
+    this.subscribeToFormChanges();
   }
 
 }

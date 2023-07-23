ebfcc81b348feca3e6df08b32d40e6dee42bbe21
tedythsu <ted.yt.hsu@gmail.com>
2023-07-23 17:11:51 +0800

    refactor: app component, fix: typo, feat: setHeaderTitle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isCollapsed = false;
+  headerTitle = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.setHeaderTitle(event.url);
+      }
+    });
+  }
+
+  private setHeaderTitle(url: string) {
+    if (url === '/login') {
+      this.headerTitle = '會員登入';
+    } else if (url === '/change-username') {
+      this.headerTitle = '設定使用者名稱';
+    } else if (url === '/change-password') {
+      this.headerTitle = '設定新密碼';
+    } else if (url === '/forgot-username-or-password') {
+      this.headerTitle = '忘記使用者名稱或密碼';
+    } else {
+      // Default Header Title
+      this.headerTitle = '';
+    }
+  }
+
+
 }
